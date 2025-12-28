cab4ad55f4a25d45761d8e34cca84e03c27e23c2
darkguyidk <93890773+darkguyidk@users.noreply.github.com>
2025-12-28 08:56:21 -0800

    Update jsmf.user.js

--- jsmf.user.js
+++ jsmf.user.js
@@ -7,5 +7,5 @@
 // @version     1
 // @namespace   https://github.com/darkguyidk/
 // @grant       none
-// @icon        https://dummyimage.com/600x600/000/fff.png&text=JSMF
+// @icon        https://camo.githubusercontent.com/12ee4ba2f5b77d76399584a88a72d5a49ef0691e152c0f3972631d551223b063/68747470733a2f2f64756d6d79696d6167652e636f6d2f333030783330302f3030302f6666662e706e6726746578743d4a534d46
 // ==/UserScript==
