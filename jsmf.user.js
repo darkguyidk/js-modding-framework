bdf26c5ab2a89ae10f03f41c9facc3aefb6010fd
darkguyidk <93890773+darkguyidk@users.noreply.github.com>
2025-12-28 08:51:30 -0800

    Add initial UserScript template for js-modding-framework

---
+++ jsmf.user.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name        js-modding-framework
+// @match       *://*.*
+// @description just a modding framework for javascript
+// @run-at      document-start
+// @license     MIT
+// @version     1
+// @namespace https://github.com/darkguyidk/
+// ==/UserScript==
